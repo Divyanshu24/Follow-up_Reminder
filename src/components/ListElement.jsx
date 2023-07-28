16132931d868d62e8f1ec7c482ce3be10a3622ae
Divyanshu <guptadivyanshu24@gmail.com>
2023-07-28 14:05:02 +0530

    Sync

--- src/components/ListElement.jsx
+++ src/components/ListElement.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 import '../styles/ListElement.css'
 const ListElement = ({ object,onSelect, id}) => {
-    const [send, setsend]=useState('SEND');
-    const [btncolor,setbtncolor]=useState("#4285F4");
+    const [send, setsend]=useState('Send');
+    const [btncolor,setbtncolor]=useState("#492769");
   return (
     <div className='list-element'>
         <li><span className='list-text'>{object.name}</span><span className='list-text'>{object.email} </span> <span className='list-text'>{object.message}</span> <span><button className='list-btn' style={{backgroundColor:btncolor}}  onClick={()=>{
             setbtncolor("#009933")
-            setsend('SENT✅');
+            setsend('Sent');
             onSelect(object)}}>{send}</button></span></li>
         
         
